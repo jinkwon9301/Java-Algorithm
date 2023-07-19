4f0691e3dc75b5a2c53d2335871896bdad0d9beb
jinkwon9301 <77374066+jinkwon9301@users.noreply.github.com>
2023-07-19 11:15:00 +0900

    [unrated] Title: 9로 나눈 나머지, Time: 0.04 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.java
+++ 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -3,15 +3,25 @@ import java.util.*;
 class Solution {
     public int solution(String number) {
         
-        String[] arr = number.split("");
+//         String[] arr = number.split("");
         
-        int sum = 0;
+//         int sum = 0;
         
-        for (String e : arr) {
-            sum += Integer.parseInt(e);
+//         for (String e : arr) {
+//             sum += Integer.parseInt(e);
+//         }
+        
+//         return sum % 9;
+
+        int answer = 0;
+
+        for(int i=0; i<number.length(); i++) {
+            answer += number.charAt(i) - '0';
         }
-        
-        return sum % 9;
+
+        answer %= 9;
+
+        return answer;
         
         
     }
