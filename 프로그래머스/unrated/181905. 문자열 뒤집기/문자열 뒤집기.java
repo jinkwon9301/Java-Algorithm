e8987d6fc50b0b70fad59f008591b954c0c1ae68
jinkwon9301 <77374066+jinkwon9301@users.noreply.github.com>
2023-07-19 12:29:36 +0900

    [unrated] Title: 문자열 뒤집기, Time: 14.35 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,15 +1,19 @@
 class Solution {
     public String solution(String my_string, int s, int e) {
         
-        String target_str = my_string.substring(s, e + 1);
-        StringBuffer target_reverse = new StringBuffer();
-        for (int i = target_str.length() - 1; i >= 0; i--) {
-            target_reverse.append(target_str.charAt(i));
-        }
+//         String target_str = my_string.substring(s, e + 1);
+//         StringBuffer target_reverse = new StringBuffer();
+//         for (int i = target_str.length() - 1; i >= 0; i--) {
+//             target_reverse.append(target_str.charAt(i));
+//         }
         
-        return my_string.substring(0, s)
-        + target_reverse.toString()
-        + my_string.substring(e + 1, my_string.length());
+//         return my_string.substring(0, s)
+//         + target_reverse.toString()
+//         + my_string.substring(e + 1, my_string.length());
+        
+        StringBuilder answer = new StringBuilder(my_string.substring(s, e + 1));
+        answer.reverse();
+        return my_string.substring(0, s) + answer + my_string.substring(e + 1);
         
     }
 }
