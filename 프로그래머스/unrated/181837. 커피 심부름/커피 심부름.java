768e8a6f1867af5dbcf821be8370dc6ea0076aa9
jinkwon9301 <77374066+jinkwon9301@users.noreply.github.com>
2023-07-21 14:39:23 +0900

    [unrated] Title: 커피 심부름, Time: 0.03 ms, Memory: 70.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(String[] order) {
+        int answer = 0;
+        
+        for (String str : order) {
+            if (str.equals("iceamericano") || 
+                str.equals("americanoice") || 
+                str.equals("hotamericano") || 
+                str.equals("americanohot") || 
+                str.equals("americano") || 
+                str.equals("anything")
+               ) answer += 4500;
+            else answer += 5000;
+        }
+        
+        return answer;
+    }
+}
