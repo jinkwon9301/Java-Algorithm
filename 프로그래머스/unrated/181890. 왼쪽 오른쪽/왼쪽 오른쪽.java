6ccea5be3326fb78253d511d9cd039e1b14a7c90
jinkwon9301 <77374066+jinkwon9301@users.noreply.github.com>
2023-07-25 10:52:27 +0900

    [unrated] Title: 왼쪽 오른쪽, Time: 0.02 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] str_list) {
+        
+        for (int i = 0; i < str_list.length; i++) {
+            if (str_list[i].equals("l")) {
+                String[] answer = new String[i];
+                for (int j = 0; j < answer.length; j++) {
+                    answer[j] = str_list[j];
+                }
+                return answer;
+            } else if (str_list[i].equals("r")) {
+                String[] answer = new String[str_list.length - (i + 1)];
+                int idx = i + 1;
+                for (int j = 0; j < answer.length; j++) {
+                    answer[j] = str_list[idx++];
+                }
+                return answer;
+            }
+        }
+        
+        String[] answer = {};
+        return answer;
+        
+    }
+}
