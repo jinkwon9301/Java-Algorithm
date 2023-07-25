9130284b8b67a302a4262299b76ed19e0a2d7ada
jinkwon9301 <77374066+jinkwon9301@users.noreply.github.com>
2023-07-25 10:54:01 +0900

    [unrated] Title: 왼쪽 오른쪽, Time: 0.03 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -3,25 +3,37 @@ import java.util.*;
 class Solution {
     public String[] solution(String[] str_list) {
         
+        
         for (int i = 0; i < str_list.length; i++) {
-            if (str_list[i].equals("l")) {
-                String[] answer = new String[i];
-                for (int j = 0; j < answer.length; j++) {
-                    answer[j] = str_list[j];
-                }
-                return answer;
-            } else if (str_list[i].equals("r")) {
-                String[] answer = new String[str_list.length - (i + 1)];
-                int idx = i + 1;
-                for (int j = 0; j < answer.length; j++) {
-                    answer[j] = str_list[idx++];
-                }
-                return answer;
+
+            if ("l".equals(str_list[i])) {
+                return Arrays.copyOfRange(str_list, 0, i);
+            } else if ("r".equals(str_list[i])) {
+                return Arrays.copyOfRange(str_list, i + 1, str_list.length);
             }
         }
+
+        return new String[0];
         
-        String[] answer = {};
-        return answer;
+//         for (int i = 0; i < str_list.length; i++) {
+//             if (str_list[i].equals("l")) {
+//                 String[] answer = new String[i];
+//                 for (int j = 0; j < answer.length; j++) {
+//                     answer[j] = str_list[j];
+//                 }
+//                 return answer;
+//             } else if (str_list[i].equals("r")) {
+//                 String[] answer = new String[str_list.length - (i + 1)];
+//                 int idx = i + 1;
+//                 for (int j = 0; j < answer.length; j++) {
+//                     answer[j] = str_list[idx++];
+//                 }
+//                 return answer;
+//             }
+//         }
+        
+//         String[] answer = {};
+//         return answer;
         
     }
 }
