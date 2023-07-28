f8589406cbec80ac4d87ae189c5b12cddc53621d
jinkwon9301 <77374066+jinkwon9301@users.noreply.github.com>
2023-07-28 11:12:39 +0900

    [unrated] Title: 배열 만들기 2, Time: 14.40 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int l, int r) {
+        List<Integer> list = new ArrayList<>();
+        int start = (l % 5 == 0) ? l : (l / 5) * 5 + 5;
+        int last = r;
+        
+        for(int i = start; i <= last; i += 5) {
+            String str = i + "";
+            int count = 0;
+            for(int j = 0; j < str.length(); j++) {
+                if(str.charAt(j) == 48 || str.charAt(j) == 53) {
+                    count ++;
+                }
+            }
+            if(count == str.length()) {
+                list.add(i);
+            }
+        }
+        int[] answer = list.stream().mapToInt(i->i).toArray();
+        int[] empty = {-1};
+        if(answer.length == 0) return empty;
+        return answer;
+    }
+}
