5a04281e277d8d7c71dd00e4c332c0ab4bdcb955
jinkwon9301 <77374066+jinkwon9301@users.noreply.github.com>
2023-08-18 09:58:10 +0900

    [unrated] Title: 기사단원의 무기, Time: 0.33 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,28 +1,48 @@
 class Solution {
+
     public int solution(int number, int limit, int power) {
-        int sum = 0;
-
+        int[] count = new int[number + 1];    
         for (int i = 1; i <= number; i++) {
-          int attack_power = divisor_count(i);
-
-          if (attack_power <= limit) sum += attack_power;
-          else sum += power;
-        }
-        
-        return sum;
-    }
-    
-    static int divisor_count(int num) {
-        int count = 0;
-
-        for (int i = 1; i <= Math.sqrt(num); i++) {
-            if (i == Math.sqrt(num)) {
-                count++;
-                break;
+            for (int j = 1; j <= number / i; j++) {
+                count[i * j]++;
             }
-            if (num % i == 0) count += 2;
         }
-        
-        return count;
-     }
+        int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            if (count[i] > limit) {
+                answer += power;
+            } else {
+                answer += count[i];
+            }
+        }
+        return answer;
+    }
 }
+// class Solution {
+//     public int solution(int number, int limit, int power) {
+//         int sum = 0;
+
+//         for (int i = 1; i <= number; i++) {
+//           int attack_power = divisor_count(i);
+
+//           if (attack_power <= limit) sum += attack_power;
+//           else sum += power;
+//         }
+        
+//         return sum;
+//     }
+    
+//     static int divisor_count(int num) {
+//         int count = 0;
+
+//         for (int i = 1; i <= Math.sqrt(num); i++) {
+//             if (i == Math.sqrt(num)) {
+//                 count++;
+//                 break;
+//             }
+//             if (num % i == 0) count += 2;
+//         }
+        
+//         return count;
+//      }
+// }
