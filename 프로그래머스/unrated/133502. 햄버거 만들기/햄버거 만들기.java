4aa2ec4f56f9151b1f831c2f789e4ce015c60f7f
jinkwon9301 <77374066+jinkwon9301@users.noreply.github.com>
2023-08-22 08:58:10 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
@@ -1,15 +1,31 @@
 class Solution {
     public int solution(int[] ingredient) {
-        StringBuffer sb = new StringBuffer();
-        int count = 0;
-        for (int i = 0; i < ingredient.length; i++) {
-          sb.append(ingredient[i]);
-          if (ingredient[i] == 1 && sb.length() >= 4 && sb.substring(sb.length() - 4, sb.length()).toString().equals("1231")) {
-            count++;
-            sb = new StringBuffer(sb.substring(0, sb.length() - 4));
-          }
-        }
         
-        return count;
+        int[] stack = new int[ingredient.length];
+        int sp = 0;
+        int answer = 0;
+        for (int i : ingredient) {
+            stack[sp++] = i;
+            if (sp >= 4 && stack[sp - 1] == 1
+                && stack[sp - 2] == 3
+                && stack[sp - 3] == 2
+                && stack[sp - 4] == 1) {
+                sp -= 4;
+                answer++;
+            }
+        }
+        return answer;
+        
+//         StringBuffer sb = new StringBuffer();
+//         int count = 0;
+//         for (int i = 0; i < ingredient.length; i++) {
+//           sb.append(ingredient[i]);
+//           if (ingredient[i] == 1 && sb.length() >= 4 && sb.substring(sb.length() - 4, sb.length()).toString().equals("1231")) {
+//             count++;
+//             sb = new StringBuffer(sb.substring(0, sb.length() - 4));
+//           }
+//         }
+        
+//         return count;
     }
 }
