d01251ea81526a9180dff17639cb9c6bbc17b71b
jinkwon9301 <77374066+jinkwon9301@users.noreply.github.com>
2023-09-27 12:15:23 +0900

    [unrated] Title: 연속된 부분 수열의 합, Time: 0.02 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -7,65 +7,67 @@
 while 문을 사용하여 sum(누적합)이 k보다 작을 때 까지 R의 값을 1씩 증가시키며 합을 구합니다. while 문이 종료된 후에, sum 값이 k와 같으면 문제의 조건에 맞게 수열의 길이를 비교하여 정답을 업데이트 해주면 문제를 해결할 수 있습니다.
 */
 
-class Solution {
-    public int[] solution(int[] sequence, int k) {
-        
-        int N = sequence.length;
-        int left = 0, right = N;
-        int sum = 0;
-        for(int L = 0, R = 0; L < N; L++) {
-            while(R < N && sum < k) {
-                sum += sequence[R++];
-            }
-            
-            if(sum == k) {
-                int range = R - L - 1;
-                if((right - left) > range) {
-                    left = L;
-                    right = R - 1;
-                }
-            }
-            
-            sum -= sequence[L];
-        }
-        
-        int[] answer = {left, right};
-        
-        return answer;
-    }
-}
-
 // class Solution {
 //     public int[] solution(int[] sequence, int k) {
-//         // 투 포인터
-//         int[] result = {0, 0};
-//         int start = 0;
-//         int end = 0;
-//         int size = sequence.length;
+        
+//         int N = sequence.length;
+//         int left = 0, right = N;
 //         int sum = 0;
-
-//         while (true) {
-//           sum = 0;
-
-//           for (int i = start; i <= end; i++) {
-//             sum += sequence[i];
-//           }
-
-//           if (sum < k) end++;
-//           else if (k < sum) start++;
-//           else if (k == sum) {
-//             if (end - start < size) {
-//               result[0] = start;
-//               result[1] = end;
-//               size = end - start;
-//               if (size == 0) break;
+//         for(int L = 0, R = 0; L < N; L++) {
+//             while(R < N && sum < k) {
+//                 sum += sequence[R++];
 //             }
-//             start++;
-//           }
-
-//           if (end == sequence.length || start == end + 1) break;
+            
+//             if(sum == k) {
+//                 int range = R - L - 1;
+//                 if((right - left) > range) {
+//                     left = L;
+//                     right = R - 1;
+//                 }
+//             }
+            
+//             sum -= sequence[L];
 //         }
-
-//         return result;
+        
+//         int[] answer = {left, right};
+        
+//         return answer;
 //     }
 // }
+
+class Solution {
+    public int[] solution(int[] sequence, int k) {
+        // 투 포인터
+        int[] result = {0, 0};
+        int start = 0;
+        int end = 0;
+        int size = sequence.length;
+        int sum = sequence[0];
+
+        while (true) {
+          if (sum < k) {
+            end++;
+            if (end == sequence.length || start == end + 1) break;
+            sum += sequence[end];
+          }
+          else if (k < sum) {
+            start++;
+            sum -= sequence[start - 1];
+          }
+          else if (k == sum) {
+            if (end - start < size) {
+              result[0] = start;
+              result[1] = end;
+              size = end - start;
+              if (size == 0) break;
+            }
+            start++;
+            sum -= sequence[start - 1];
+          }
+
+          if (end == sequence.length || start == end + 1) break;
+        }
+
+        return result;
+    }
+}
