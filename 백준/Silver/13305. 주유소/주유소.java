c2b053c8501389196d7cdd487ce3754a3421a9b1
jinkwon9301 <77374066+jinkwon9301@users.noreply.github.com>
2024-01-05 11:30:13 +0900

    [Silver III] Title: 주유소, Time: 584 ms, Memory: 66340 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -8,38 +9,26 @@ public class Main {
     int N = Integer.parseInt(br.readLine());
     StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-    int[] distances = new int[N - 1];
+    BigInteger[] distances = new BigInteger[N - 1];
     for (int i = 0; i < distances.length; i++) {
-      distances[i] = Integer.parseInt(st.nextToken());
+      distances[i] = new BigInteger(st.nextToken());
     }
 
     st = new StringTokenizer(br.readLine(), " ");
-    int[] cities = new int[N];
-    for (int i = 0; i < cities.length; i++) {
-      cities[i] = Integer.parseInt(st.nextToken());
+    BigInteger[] oil = new BigInteger[N];
+    for (int i = 0; i < oil.length; i++) {
+      oil[i] = new BigInteger(st.nextToken());
     }
 
-    // 최소 기름값인 도시의 인덱스와 기름값을 담을 배열
-    // 초기화
-    int[] min = {0, cities[0]};
+    BigInteger total = oil[0].multiply(distances[0]);
+    BigInteger minPrice = oil[0];
 
-    int total = 0;
-    int distance = 0;
-
-    // 도시 기름값 배열을 순회한다.
-    for (int i = 1; i < cities.length; i++) {
-      // 만약 다음 도시의 기름값이 최소 기름값 보다 작다면 || i가 마지막 인덱스라면
-      if (cities[i] < min[1] || i == cities.length - 1) {
-        // 그 도시까지의 거리 * 기존 최소 기름값
-        for (int j = min[0]; j < i; j++) {
-          distance += distances[j];
-        }
-        total += distance * min[1];
-
-        // 거리 초기화
-        distance = 0;
-        // min 갱신
-        min = new int[]{i, cities[i]};
+    for (int i = 1; i < oil.length - 1; i++) {
+      if (oil[i].compareTo(minPrice) < 0) {
+        total = total.add(distances[i].multiply(oil[i]));
+        minPrice = oil[i];
+      } else {
+        total = total.add(distances[i].multiply(minPrice));
       }
     }
 
