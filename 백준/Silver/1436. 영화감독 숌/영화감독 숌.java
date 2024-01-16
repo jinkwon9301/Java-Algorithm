85911e5b15a858f748e7d1b26c53c70306f0eacc
jinkwon9301 <77374066+jinkwon9301@users.noreply.github.com>
2024-01-16 12:06:55 +0900

    [Silver V] Title: 영화감독 숌, Time: 320 ms, Memory: 87984 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -1,20 +1,23 @@
-import java.util.Scanner;
- 
+import java.io.*;
+
 public class Main {
-	public static void main(String[] args) {
-		
-		Scanner in = new Scanner(System.in);
-		int N = in.nextInt();
- 
-		int num = 666;
-		int count = 1;
-        
-		while(count != N) {
-			num++;
-			if(String.valueOf(num).contains("666")) {
-				count++;
-			}
-		}
-		System.out.println(num);
-	}
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int N = Integer.parseInt(br.readLine());
+
+    int num = 666;
+    int count = 1;
+
+    while (count != N) {
+      num++;
+      if (String.valueOf(num).contains("666")) {
+        count++;
+      }
+    }
+
+    bw.write(num + "\n");
+    bw.flush();
+  }
 }
