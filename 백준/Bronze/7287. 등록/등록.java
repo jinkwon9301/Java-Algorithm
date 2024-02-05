6cf561e0714881dbccbe6f0474d95ada85dff3ef
jinkwon9301 <77374066+jinkwon9301@users.noreply.github.com>
2024-02-05 17:08:52 +0900

    [Bronze V] Title: 등록, Time: 120 ms, Memory: 13964 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    // 입력 값 받기
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    System.out.println(168);
+    System.out.println("kjg9301");
+  }
+}
