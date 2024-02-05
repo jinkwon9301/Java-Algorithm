cd03f15453a56331177597d83ad63eae6374f8bb
jinkwon9301 <77374066+jinkwon9301@users.noreply.github.com>
2024-02-05 17:10:47 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 14076 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    // 입력 값 받기
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    System.out.println("         ,r'\"7");
+    System.out.println("r`-_   ,'  ,/");
+    System.out.println(" \\. \". L_r'");
+    System.out.println("   `~\\/");
+    System.out.println("      |");
+    System.out.println("      |");
+  }
+}
