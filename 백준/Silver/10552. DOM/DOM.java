a080a0bc128d82a9ab498171681113c39c10d3af
JINKWON <77374066+jinkwon9301@users.noreply.github.com>
2024-02-11 16:44:12 +0900

    [Silver II] Title: DOM, Time: 368 ms, Memory: 44260 KB -BaekjoonHub

--- 백준/Silver/10552. DOM/DOM.java
+++ 백준/Silver/10552. DOM/DOM.java
@@ -1,35 +1,58 @@
-import java.util.*;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int count = 0;
-    static int[] toFavouriteCH = new int[100000 + 1];
-    static boolean[] visited = new boolean[100000 + 1];
-    static public void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+  static int N, M, P;
+  static int[] channel;
+  // 1 → 2 → 3 → 1 → 2 → 3 이렇게 순환안하고
+  // 1 → 2 → 3 → 2 → 3 이렇게 순환할 수도 있기 때문에
+  // visited 배열로 순환하는지 검증해야 한다.
+  static boolean[] visited;
+  static StringTokenizer st;
 
-        int n = sc.nextInt(); // 시청자 수
-        int m = sc.nextInt(); // 채널 수 1 to m
-        int p = sc.nextInt(); // 최초 채널
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+    st = new StringTokenizer(br.readLine(), " ");
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+    P = Integer.parseInt(st.nextToken());
 
-        while(--n >= 0) {
-            int favouriteCH = sc.nextInt();
-            int hatedCH = sc.nextInt();
+    // 그래프 초기화 (채널들을 정점으로 그래프 만든다.)
+    channel = new int[M + 1];
+    visited = new boolean[M + 1];
 
-            if (toFavouriteCH[hatedCH] == 0) toFavouriteCH[hatedCH] = favouriteCH;
-        }
+    for (int i = 0; i < N; i++) {
+      st = new StringTokenizer(br.readLine(), " ");
+      int favorite = Integer.parseInt(st.nextToken());
+      int hate = Integer.parseInt(st.nextToken());
 
-        changeChannel(p);
-
-        System.out.print(count);
+      // 싫어하는 채널에서 좋아하는 채널로 바꿀 수 있도록 그래프를 완성해준다.
+      // (channel index : 싫어하는 채널 → value : 좋아하는 채널)
+      // 만약 특정 채널을 싫어하는 사람들이 많다면 가장 어린 애가 좋아하는 채널이 입력되도록
+      if (channel[hate] == 0) {
+        channel[hate] = favorite;
+      }
     }
 
-    static public void changeChannel(int ch) {
-        if (visited[ch]) count = -1;
-        else if (toFavouriteCH[ch] != 0) {
-            count++;
-            visited[ch] = true;
-            changeChannel(toFavouriteCH[ch]);
-        }
+    dfs(P, 0);
+  }
+
+  private static void dfs(int now, int count) {
+    // 종료조건
+    // 지금 이 채널을 싫어하는 사람이 없다면 채널변경 횟수(count) 보여주고 중단한다.
+    if(channel[now] == 0) {
+      System.out.println(count);
+      return;
     }
+
+    // 무한순환하는 경우에는 -1 보여주고 중단한다.
+    if (visited[now]) {
+      System.out.println(-1);
+      return;
+    }
+    visited[now] = true;
+    dfs(channel[now], count + 1);
+  }
 }
