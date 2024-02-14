57318a44a4bec4ed375889d0cff791a846f0b928
JINKWON <77374066+jinkwon9301@users.noreply.github.com>
2024-02-14 11:04:38 +0900

    [Silver I] Title: 영역 구하기, Time: 168 ms, Memory: 17256 KB -BaekjoonHub

---
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -0,0 +1,95 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+  static int M, N, K, count;
+  static boolean[][] map;
+  static boolean[][] visited;
+  static int[] dx = {0, 0, 1, -1};
+  static int[] dy = {1, -1, 0, 0};
+  static List<Integer> list;
+  static StringTokenizer st;
+  static String result = "";
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    st = new StringTokenizer(br.readLine(), " ");
+    M = Integer.parseInt(st.nextToken());
+    N = Integer.parseInt(st.nextToken());
+    K = Integer.parseInt(st.nextToken());
+
+    // 일단 map의 모든 영역을 true로 표기한다.
+    map = new boolean[M][N];
+    for (int i = 0; i < M; i++) {
+      for (int j = 0; j < N; j++) {
+        map[i][j] = true;
+      }
+    }
+
+    // visited 배열과 분리된 영역의 크기를 담을 list 초기화
+    visited = new boolean[M][N];
+    list = new ArrayList<>();
+
+    // 직사각형 영역들을 false로 바꿔준다.
+    for (int i = 0; i < K; i++) {
+      st = new StringTokenizer(br.readLine(), " ");
+
+      int y1 = Integer.parseInt(st.nextToken());
+      int x1 = Integer.parseInt(st.nextToken());
+      int y2 = Integer.parseInt(st.nextToken());
+      int x2 = Integer.parseInt(st.nextToken());
+
+      for (int j = x1; j < x2; j++) {
+        for (int k = y1; k < y2; k++) {
+          map[j][k] = false;
+        }
+      }
+    }
+
+    // dfs 탐색
+    for (int i = 0; i < M; i++) {
+      for (int j = 0; j < N; j++) {
+        count = 0;
+        if (!visited[i][j] && map[i][j]) {
+          dfs(i, j);
+          list.add(count);
+        }
+      }
+    }
+
+    // 정렬
+    Collections.sort(list);
+
+    // 출력
+    System.out.println(list.size());
+    for (Integer apartments : list) {
+      result += apartments + " ";
+    }
+    System.out.println(result.substring(0, result.length() - 1));
+  }
+
+  private static void dfs(int x, int y) {
+    visited[x][y] = true;
+    count++;
+
+    for (int i = 0; i < 4; i++) {
+      int nx = x + dx[i];
+      int ny = y + dy[i];
+      if (validBound(nx, ny) && !visited[nx][ny] && map[nx][ny]) {
+        dfs(nx, ny);
+      }
+    }
+  }
+
+  private static boolean validBound(int nx, int ny) {
+    if (0 <= nx && nx < M && 0 <= ny && ny < N) {
+      return true;
+    }
+    return false;
+  }
+}
