d8847cdfc7a7d476034ea34f3cc88ce8585398eb
JINKWON <77374066+jinkwon9301@users.noreply.github.com>
2024-12-22 22:53:39 +0900

    [level 3] Title: 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
+++ 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
@@ -16,3 +16,36 @@ LEFT JOIN (
     AND '2022-10-16' <= END_DATE) T2
 ON T1.CAR_ID = T2.CAR_ID
 ORDER BY T1.CAR_ID DESC
+
+-- 1. EXIST
+# SELECT 
+#     DISTINCT C.CAR_ID,
+#     CASE
+#         WHEN EXISTS (
+#             SELECT 1
+#             FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY R
+#             WHERE R.CAR_ID = C.CAR_ID
+#               AND R.START_DATE <= DATE'2022-10-16'
+#               AND DATE'2022-10-16' <= R.END_DATE
+#         ) THEN '대여중'
+#         ELSE '대여 가능'
+#     END AS AVAILABILITY
+# FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY C
+# ORDER BY C.CAR_ID DESC;
+
+-- 2. 서브쿼리를 이용한 컬럼비교
+# SELECT 
+#     DISTINCT C.CAR_ID,
+#     CASE
+#         WHEN ( 
+#             SELECT COUNT(*) 
+#             FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY R
+#             WHERE R.CAR_ID = C.CAR_ID
+#               AND R.START_DATE <= DATE'2022-10-16'
+#               AND DATE'2022-10-16' <= R.END_DATE
+#         ) > 0
+#         THEN '대여중'
+#         ELSE '대여 가능'
+#     END AS AVAILABILITY
+# FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY C
+# ORDER BY C.CAR_ID DESC;
