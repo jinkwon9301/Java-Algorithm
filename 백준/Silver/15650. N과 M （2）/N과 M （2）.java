685aa750d131ae4443720d78e3664211629229f8
JINKWON <77374066+jinkwon9301@users.noreply.github.com>
2025-04-13 08:11:39 +0900

    [Silver III] Title: N과 M (2), Time: 120 ms, Memory: 15844 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+  static int N, M;
+  static int[] arr;
+  static boolean[] visited;
+  static StringTokenizer st;
+//  static StringBuilder sb = new StringBuilder();
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+
+    arr = new int[N];
+    visited = new boolean[N];
+
+    dfs(0);
+  }
+
+  private static void dfs(int depth) {
+    // 종료 조건
+    if (depth == M) {
+      for (int i = 0; i < M; i++) {
+        System.out.print(arr[i] + " ");
+      }
+      System.out.println();
+      return;
+    }
+
+    // 재귀 호출
+    for (int i = 0; i < N; i++) {
+      if (!visited[i]) {
+        if (depth != 0 && arr[depth - 1] > i + 1) {
+          continue;
+        }
+        visited[i] = true;
+        arr[depth] = i + 1;
+        dfs(depth + 1);
+        visited[i] = false;
+      }
+    }
+  }
+}
