04df0de584d3f2b76417089595dedf564af8b87f
JINKWON <77374066+jinkwon9301@users.noreply.github.com>
2025-04-19 18:45:47 +0900

    [level 1] Title: [1차] 비밀지도, Time: 7.45 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,4 +1,17 @@
-  class Solution {
+/*
+[비트기반 연산]
+
+|는 겹쳐서 하나라도 1이면 1
+&는 둘 다 1일 때만 1
+^는 서로 다를 때 1
+
+String.format("%ns", str)
+%	포맷 시작
+n	전체 너비 (문자 수)
+s	문자열(String)
+
+*/
+class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
 
       String[] map = new String[n];
